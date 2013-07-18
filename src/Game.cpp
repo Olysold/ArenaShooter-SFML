10e98f5bb7e3048bb9b4ab6cc6decaddd017a2f9
Zephilinox <zephilinox@lavabit.com>
2013-07-18 15:19:03 +0100

    Debug Collision (Recommend LocalBounds)

--- src/Game.cpp
+++ src/Game.cpp
@@ -51,6 +51,36 @@ void Game::drawEntity(Player& player,
     {
         arena.draw(window);
 
+        ///Conclusion: GlobalBounds collision doesn't take in to account rotation (look at dark red player vs enemy), we should use LocalBounds or it will be innacurate
+
+        //Debug what player's global bounds looks like
+        sf::RectangleShape debugP;
+
+        debugP.setSize(sf::Vector2f(player.sprite.getGlobalBounds().width, player.sprite.getGlobalBounds().height));
+        debugP.setOrigin(debugP.getSize().x / 2.f, debugP.getSize().y / 2.f);
+        debugP.setPosition(player.sprite.getPosition());
+        debugP.setRotation(player.sprite.getRotation());
+        debugP.setFillColor(sf::Color(255, 0, 0));
+
+        window.draw(debugP);
+
+        //Debug player global bounds, without rotation.
+        sf::RectangleShape debugPA = debugP;
+        debugPA.setRotation(0);
+        debugPA.setFillColor(sf::Color(100, 0, 0));
+
+        window.draw(debugPA);
+
+        //Debug what player's local bounds looks like
+        sf::RectangleShape debugP2;
+        debugP2.setSize(sf::Vector2f(player.sprite.getLocalBounds().width, player.sprite.getLocalBounds().height));
+        debugP2.setOrigin(debugP2.getSize().x / 2.f, debugP2.getSize().y / 2.f);
+        debugP2.setPosition(player.sprite.getPosition());
+        debugP2.setRotation(player.sprite.getRotation());
+        debugP2.setFillColor(sf::Color(0, 255, 0));
+
+        window.draw(debugP2);
+
         for (auto bullet : bulMan.getEnemyBullets())
         {
             bullet.draw(window);
@@ -63,6 +93,32 @@ void Game::drawEntity(Player& player,
 
         for (auto enemy : enemyMan.getEnemies())
         {
+            //Debug what enemies global bounds looks like
+            sf::RectangleShape debugP;
+
+            debugP.setSize(sf::Vector2f(enemy.sprite.getGlobalBounds().width, enemy.sprite.getGlobalBounds().height));
+            debugP.setOrigin(debugP.getSize().x / 2.f, debugP.getSize().y / 2.f);
+            debugP.setPosition(enemy.sprite.getPosition());
+            debugP.setRotation(enemy.sprite.getRotation());
+            debugP.setFillColor(sf::Color(255, 0, 0));
+
+            window.draw(debugP);
+
+            //Debug enemies global bounds, without rotation.
+            sf::RectangleShape debugPA = debugP;
+            debugPA.setRotation(0);
+            debugPA.setFillColor(sf::Color(100, 0, 0));
+
+            window.draw(debugPA);
+
+            //Debug what enemies local bounds looks like
+            sf::RectangleShape debugP2;
+            debugP2.setSize(sf::Vector2f(enemy.sprite.getLocalBounds().width, enemy.sprite.getLocalBounds().height));
+            debugP2.setOrigin(debugP2.getSize().x / 2.f, debugP2.getSize().y / 2.f);
+            debugP2.setPosition(enemy.sprite.getPosition());
+            debugP2.setRotation(enemy.sprite.getRotation());
+            debugP2.setFillColor(sf::Color(0, 255, 0));
+
             enemy.draw(window);
         }
 
